309e27828c7779adaa1bfe48a61d798f8e78b749
Kuzminykh A.O. Home <a.kuzminykh@saures.ru>
2021-05-23 17:57:01 +0300

    Opening, reading, closing file in loop has been implemented.

--- cmd/ocp-classroom-api/main.go
+++ cmd/ocp-classroom-api/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello World!\r\nI'm ocp-classroom-api package by Aleksandr Kuzminykh.")
+
+	openReadCloseFile := func(i int) {
+
+		file, err := os.OpenFile("hello.txt", os.O_RDONLY|os.O_APPEND|os.O_CREATE, 0666)
+
+		if err != nil {
+			fmt.Println("Unable to create file:", err)
+			os.Exit(1)
+		}
+
+		defer file.Close()
+
+		var bytes []byte = make([]byte, 1024)
+
+		_, err = file.Read(bytes)
+
+		fmt.Println("Reading file for", i+1, "th time:", string(bytes))
+
+		if err != nil {
+			fmt.Println("Unable to write to file:", err)
+			os.Exit(1)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	for i := 0; i < 10; i++ {
+
+		openReadCloseFile(i)
+	}
 }
