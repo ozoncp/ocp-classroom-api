666489a4abaa474199a9e38bc26220733189a6b2
Kuzminykh A.O. Home <a.kuzminykh@saures.ru>
2021-06-02 00:53:39 +0300

    Improve Flusher implementation. Add new tests for Flusher.

--- internal/flusher/flusher_test.go
+++ internal/flusher/flusher_test.go
@@ -14,48 +14,93 @@ import (
 
 var _ = Describe("Flusher", func() {
 
-	var (
-		ctrl     *gomock.Controller
-		mockRepo *mocks.MockRepo
+	Describe("Flush call", func() {
 
-		fl flusher.Flusher
-	)
+		const chunkSize int = 2
 
-	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
-		mockRepo = mocks.NewMockRepo(ctrl)
+		var (
+			ctrl     *gomock.Controller
+			mockRepo *mocks.MockRepo
 
-		fl = flusher.New(mockRepo)
-	})
+			classrooms []models.Classroom
+			fl         flusher.Flusher
+		)
 
-	AfterEach(func() {
-		ctrl.Finish()
-	})
+		BeforeEach(func() {
+			ctrl = gomock.NewController(GinkgoT())
+			mockRepo = mocks.NewMockRepo(ctrl)
 
-	Context("Flush call", func() {
-
-		It("can flush", func() {
-
-			mockRepo.EXPECT().AddClassrooms(gomock.Any()).Return(nil)
-
-			classrooms := []models.Classroom{}
-			remainingClassrooms := fl.Flush(classrooms)
-
-			Expect(remainingClassrooms).Should(BeNil())
+			classrooms = []models.Classroom{
+				{Id: 1, TenantId: 1, CalendarId: 1},
+				{Id: 2, TenantId: 2, CalendarId: 2},
+				{Id: 3, TenantId: 3, CalendarId: 3},
+				{Id: 4, TenantId: 4, CalendarId: 4},
+				{Id: 5, TenantId: 5, CalendarId: 5},
+			}
 		})
 
-		It("can not flush", func() {
+		AfterEach(func() {
+			ctrl.Finish()
+		})
 
-			mockRepo.EXPECT().AddClassrooms(gomock.Any()).DoAndReturn(
-				func(classrooms []models.Classroom) error {
-					return errors.New("can not add classrooms")
-				},
-			)
+		When("parameters are not valid", func() {
 
-			classrooms := []models.Classroom{}
-			remainingClassrooms := fl.Flush(classrooms)
+			It("should return whole slice", func() {
 
-			Expect(remainingClassrooms).Should(BeEquivalentTo(classrooms))
+				By("receiving wrong chunkSize")
+
+				fl = flusher.New(mockRepo, -1)
+				remainingClassrooms := fl.Flush(classrooms)
+
+				Expect(remainingClassrooms).Should(BeEquivalentTo(classrooms))
+
+				By("receiving nil slice of classroom")
+
+				classrooms = nil
+
+				fl = flusher.New(mockRepo, chunkSize)
+				remainingClassrooms = fl.Flush(classrooms)
+
+				Expect(remainingClassrooms).Should(BeEquivalentTo(classrooms))
+			})
+		})
+
+		When("parameters are valid", func() {
+
+			BeforeEach(func() {
+
+				fl = flusher.New(mockRepo, chunkSize)
+			})
+
+			It("flushes successfully", func() {
+
+				mockRepo.EXPECT().AddClassrooms(gomock.Any()).Times(3).Return(nil)
+
+				remainingClassrooms := fl.Flush(classrooms)
+
+				Expect(remainingClassrooms).Should(BeNil())
+			})
+
+			It("can not flush fully", func() {
+
+				gomock.InOrder(
+					mockRepo.EXPECT().AddClassrooms(gomock.Any()).Return(nil),
+					mockRepo.EXPECT().AddClassrooms(gomock.Any()).Return(errors.New("can not add classrooms")),
+				)
+
+				remainingClassrooms := fl.Flush(classrooms)
+
+				Expect(remainingClassrooms).Should(BeEquivalentTo(classrooms[chunkSize:]))
+			})
+
+			It("can not flush anything", func() {
+
+				mockRepo.EXPECT().AddClassrooms(gomock.Any()).Return(errors.New("can not add classrooms"))
+
+				remainingClassrooms := fl.Flush(classrooms)
+
+				Expect(remainingClassrooms).Should(BeEquivalentTo(classrooms))
+			})
 		})
 	})
 })
