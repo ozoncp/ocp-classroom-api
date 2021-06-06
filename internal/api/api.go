ef14df32340ef3e51b399024b7d7ee8a90263c0c
Kuzminykh A.O. Home <a.kuzminykh@saures.ru>
2021-06-06 21:56:27 +0300

    Add all methods from API as unimplemented

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,19 +3,22 @@ package api
 import (
 	"context"
 
-	desc "github.com/ozoncp/ocp-classroom-api/pkg/ocp-classroom-api"
+	grpcApi "github.com/ozoncp/ocp-classroom-api/pkg/ocp-classroom-api"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-const errClassroomNotFound = "classroom not found"
+const (
+	errClassroomNotFound        = "classroom not found"
+	errClassroomCannotBeCreated = "classroom not created"
+)
 
 type api struct {
-	desc.UnimplementedOcpClassroomApiServer
+	grpcApi.UnimplementedOcpClassroomApiServer
 }
 
-func (a *api) DescribeClassroomV1(ctx context.Context,
-	req *desc.DescribeClassroomV1Request) (*desc.DescribeClassroomV1Response, error) {
+func (a *api) ListClassroomsV1(ctx context.Context,
+	req *grpcApi.ListClassroomsV1Request) (*grpcApi.ListClassroomsV1Response, error) {
 
 	if err := req.Validate(); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
@@ -25,6 +28,39 @@ func (a *api) DescribeClassroomV1(ctx context.Context,
 	return nil, err
 }
 
-func NewOcpClassroomApi() desc.OcpClassroomApiServer {
+func (a *api) DescribeClassroomV1(ctx context.Context,
+	req *grpcApi.DescribeClassroomV1Request) (*grpcApi.DescribeClassroomV1Response, error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err := status.Error(codes.NotFound, errClassroomNotFound)
+	return nil, err
+}
+
+func (a *api) CreateClassroomV1(ctx context.Context,
+	req *grpcApi.CreateClassroomV1Request) (*grpcApi.CreateClassroomV1Response, error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err := status.Error(codes.NotFound, errClassroomCannotBeCreated)
+	return nil, err
+}
+
+func (a *api) RemoveClassroomV1(ctx context.Context,
+	req *grpcApi.RemoveClassroomV1Request) (*grpcApi.RemoveClassroomV1Response, error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err := status.Error(codes.NotFound, errClassroomNotFound)
+	return nil, err
+}
+
+func NewOcpClassroomApi() grpcApi.OcpClassroomApiServer {
 	return &api{}
 }
