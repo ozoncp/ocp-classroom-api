0d19c1b68989747b7ac62ef0d9f17c148c4abfb8
Kuzminykh A.O. Home <a.kuzminykh@saures.ru>
2021-06-07 22:42:46 +0300

    Add logging as implementation of grpc api

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	grpcApi "github.com/ozoncp/ocp-classroom-api/pkg/ocp-classroom-api"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -21,8 +22,16 @@ func (a *api) ListClassroomsV1(ctx context.Context,
 	req *grpcApi.ListClassroomsV1Request) (*grpcApi.ListClassroomsV1Response, error) {
 
 	if err := req.Validate(); err != nil {
+
+		log.Err(err).Msg("request can not be validated")
+
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+
+	log.Debug().
+		Uint64("Limit", req.Limit).
+		Uint64("Offset", req.Offset).
+		Msg("ListClassroomV1 call")
 
 	err := status.Error(codes.NotFound, errClassroomNotFound)
 	return nil, err
@@ -32,8 +41,16 @@ func (a *api) DescribeClassroomV1(ctx context.Context,
 	req *grpcApi.DescribeClassroomV1Request) (*grpcApi.DescribeClassroomV1Response, error) {
 
 	if err := req.Validate(); err != nil {
+
+		log.Err(err).Msg("request can not be validated")
+
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+
+	log.Debug().
+		Uint64("ClassroomId", req.ClassroomId).
+		Bool("Verbose", req.Verbose).
+		Msg("DescribeClassroomV1 call")
 
 	err := status.Error(codes.NotFound, errClassroomNotFound)
 	return nil, err
@@ -43,8 +60,16 @@ func (a *api) CreateClassroomV1(ctx context.Context,
 	req *grpcApi.CreateClassroomV1Request) (*grpcApi.CreateClassroomV1Response, error) {
 
 	if err := req.Validate(); err != nil {
+
+		log.Err(err).Msg("request can not be validated")
+
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+
+	log.Debug().
+		Uint64("TenantId", req.TenantId).
+		Uint64("CalendarId", req.CalendarId).
+		Msg("CreateClassroomV1 call")
 
 	err := status.Error(codes.NotFound, errClassroomCannotBeCreated)
 	return nil, err
@@ -54,8 +79,15 @@ func (a *api) RemoveClassroomV1(ctx context.Context,
 	req *grpcApi.RemoveClassroomV1Request) (*grpcApi.RemoveClassroomV1Response, error) {
 
 	if err := req.Validate(); err != nil {
+
+		log.Err(err).Msg("request can not be validated")
+
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+
+	log.Debug().
+		Uint64("ClassroomId", req.ClassroomId).
+		Msg("RemoveClassroomV1 call")
 
 	err := status.Error(codes.NotFound, errClassroomNotFound)
 	return nil, err
